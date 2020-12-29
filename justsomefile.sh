759168575d4bce4f7ace9845440ccf94e61998e6
TestManual (Test) <testmanual@blubracket.com>
2020-12-28 17:29:19 -0800

    Update justsomefile.sh

--- justsomefile.sh
+++ justsomefile.sh
@@ -9,3 +9,5 @@ another_password="9675ouyfOUYF*&$"
 onemore_password="98764(&^iuyKUYTTF"
 
 maurice_password="BenIsThe#1!"
+
+122820_password = "9765IUTYRktirI76rI76$Ri97ER*&^%4(75ER8^%EiytEDi^%4eIytECu756e87cvIU6rIC&6Eo7I^RVI&^RVCix75excI&6ROIu6E8X%C"
