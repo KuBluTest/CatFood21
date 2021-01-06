2a3eb3127f0b175225320c397ff1e07376e8d9d6
Nehal Agrawal <52674054+nehalagrawal-blu@users.noreply.github.com>
2021-01-05 16:00:53 -0800

    code-copy-commit

---
+++ token.py
@@ -0,0 +1,15 @@
+github_token = 'e442cf825b0294f35f01390c19f42759e7f15024'
+
+Master instance
+Slave instance
+
+Blackbox
+Whitebox
+
+Blacklist
+Whitelist
+
+3 man hours
+1 man day
+
+automation@blubracket.com
